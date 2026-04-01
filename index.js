8386faeff83fbfddd9fffdc7dfa55a517b91b1b5
Sabareesh001 <sabareeshcomp@gmail.com>
2026-04-01 18:16:24 +0530

    second change

---
+++ index.js
@@ -0,0 +1,10 @@
+// Task 6 - Filler Code
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+console.log(greet("World"));
+
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((n) => n * 2);
+console.log(doubled);
